aedb154514e73fdee61ea90e476450e425f41550
Son Jisu <s.zisu0417@gmail.com>
2023-04-13 00:13:32 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,15 @@
+def test():
+    score = int(input())
+    if score >= 90:
+        print('A')
+    elif score >= 80:
+        print('B')
+    elif score >= 70:
+        print('C')
+    elif score >= 60:
+        print('D')
+    else:
+        print('F')
+    return
+
+test()
